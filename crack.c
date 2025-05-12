aba3f5b42a003fd697f65133b3faa25a66c8767e
Miles Boyle <mboyle3@student.sierracollege.edu>
2025-05-11 19:54:02 -0700

    Fix from level 10 implemented, all is functional

--- crack.c
+++ crack.c
@@ -25,24 +25,53 @@ int main(int argc, char *argv[])
     //   Use the loadFile function from fileutil.c
     //   Uncomment the appropriate statement.
     int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
+    char (*hashes)[HASH_LEN] = loadFile2D(argv[1], &size);
     //char **hashes = loadFile(argv[1], &size);
     
     // CHALLENGE1: Sort the hashes using qsort.
     
     // TODO
     // Open the password file for reading.
+    FILE *in = fopen(argv[2], "r");
+    if (!in)
+	{
+	    perror("Can't open file");
+	    exit(1);
+	}
+
+    // initialize variables
+    char * password = malloc(sizeof(char[PASS_LEN]));
+    int passCount = 0;
 
     // TODO
     // For each password, hash it, then use the array search
     // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
-    // CHALLENGE1: Use binary search instead of linear search.
+    while(fgets(password, PASS_LEN, in) != NULL)
+	{
+        // Trim newline
+        char *newline = strchr(password, '\n');
+		if(newline) *newline = '\0';
 
+        char * target = md5(password, strlen(password) );
+
+        char * found = substringSearch2D(target, hashes, size);
+
+        if(found)
+        {
+            // If you find it, display the password and the hash.
+            printf("%20s | %s\n", password, found);
+            // Keep track of how many hashes were found.
+            passCount++;
+            // CHALLENGE1: Use binary search instead of linear search.
+        }
+    }
     // TODO
     // When done with the file:
     //   Close the file
+    fclose(in);
     //   Display the number of hashes found.
+    printf("%d hashes found.\n", passCount);
     //   Free up memory.
+    free(password);
+    free2D(hashes);
 }
